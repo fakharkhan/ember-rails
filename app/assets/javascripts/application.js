75bdb795dfe4a903d412b49e8a62228bcdb8c7d3
Fakhar Khan <fakhar@softpyramid.com>
2013-11-12 15:39:08 +0500

    Done with FixtureAdapter

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,24 +1,170 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// the compiled file.
-//
-// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
-// GO AFTER THE REQUIRES BELOW.
-//
-//= require jquery
-//= require jquery_ujs
-//= require lib/handlebars
-//= require lib/ember
-//= require lib/ember-data
-
-
 window.Todos = Ember.Application.create();
 
+Todos.ApplicationAdapter = DS.FixtureAdapter.extend();
+
+
 Todos.Router.map(function () {
-    this.resource('todos', { path: '/' });
+    this.resource('todos', { path: '/' }, function () {
+        // additional child routes
+        this.route('active');
+        this.route('completed');
+    });
 });
+
+
+Todos.Todo = DS.Model.extend({
+    title: DS.attr('string'),
+    isCompleted: DS.attr('boolean')
+});
+
+Todos.Todo.FIXTURES = [
+    {
+        id: 1,
+        title: 'Learn Ember.js',
+        isCompleted: true
+    },
+    {
+        id: 2,
+        title: '...',
+        isCompleted: false
+    },
+    {
+        id: 3,
+        title: 'Profit!',
+        isCompleted: false
+    }
+];
+
+Todos.TodosRoute = Ember.Route.extend({
+    model: function () {
+        return this.store.find('todo');
+    }
+});
+
+Todos.TodosIndexRoute = Ember.Route.extend({
+    model: function () {
+        return this.modelFor('todos');
+    }
+});
+
+Todos.TodosActiveRoute = Ember.Route.extend({
+    model: function(){
+        return this.store.filter('todo', function (todo) {
+            return !todo.get('isCompleted');
+        });
+    },
+    renderTemplate: function(controller){
+        this.render('todos/index', {controller: controller});
+    }
+});
+
+Todos.TodosCompletedRoute = Ember.Route.extend({
+    model: function(){
+        return this.store.filter('todo', function (todo) {
+            return todo.get('isCompleted');
+        });
+    },
+    renderTemplate: function(controller){
+        this.render('todos/index', {controller: controller});
+    }
+});
+
+Todos.TodosController = Ember.ArrayController.extend({
+    actions: {
+        createTodo: function () {
+            // Get the todo title set by the "New Todo" text field
+            var title = this.get('newTitle');
+            if (!title.trim()) { return; }
+
+            // Create the new Todo model
+            var todo = this.store.createRecord('todo', {
+                title: title,
+                isCompleted: false
+            });
+
+            // Clear the "New Todo" text field
+            this.set('newTitle', '');
+
+            // Save the new model
+            todo.save();
+        },
+        clearCompleted: function () {
+            var completed = this.filterProperty('isCompleted', true);
+            completed.invoke('deleteRecord');
+            completed.invoke('save');
+        }
+    },
+    remaining: function () {
+        return this.filterBy('isCompleted', false).get('length');
+    }.property('@each.isCompleted'),
+
+    inflection: function () {
+        var remaining = this.get('remaining');
+        return remaining === 1 ? 'item' : 'items';
+    }.property('remaining'),
+    hasCompleted: function () {
+        return this.get('completed') > 0;
+    }.property('completed'),
+
+    completed: function () {
+        return this.filterProperty('isCompleted', true).get('length');
+    }.property('@each.isCompleted'),
+    allAreDone: function (key, value) {
+        if (value === undefined) {
+            return !!this.get('length') && this.everyBy('isCompleted', true);
+        } else {
+            this.setEach('isCompleted', value);
+            this.invoke('save');
+            return value;
+        }
+    }.property('@each.isCompleted')
+
+
+});
+
+Todos.TodoController = Ember.ObjectController.extend({
+    actions: {
+        removeTodo: function () {
+            var todo = this.get('model');
+            todo.deleteRecord();
+            todo.save();
+        },
+        editTodo: function () {
+            this.set('isEditing', true);
+        } ,
+        acceptChanges: function () {
+            this.set('isEditing', false);
+
+            if (Ember.isEmpty(this.get('model.title'))) {
+                this.send('removeTodo');
+            } else {
+                this.get('model').save();
+            }
+        }
+    },
+
+    isEditing: false,
+
+    isCompleted: function(key, value){
+        var model = this.get('model');
+
+        if (value === undefined) {
+            // property being used as a getter
+            return model.get('isCompleted');
+        } else {
+            // property being used as a setter
+            model.set('isCompleted', value);
+            model.save();
+            return value;
+        }
+    }.property('model.isCompleted')
+});
+
+Todos.EditTodoView = Ember.TextField.extend({
+    didInsertElement: function () {
+        this.$().focus();
+    }
+});
+
+Ember.Handlebars.helper('edit-todo', Todos.EditTodoView);
+
